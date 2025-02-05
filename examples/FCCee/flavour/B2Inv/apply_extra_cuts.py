10a21d2b2e9d3a1524b782ef76780c0ad5bfe402
Ella Wood <ella.wood@mac.com>
2025-02-05 02:27:51 +0000

    Update script to add cuts so can deal with root files with no events (eg. for mumu and ee)

--- examples/FCCee/flavour/B2Inv/apply_extra_cuts.py
+++ examples/FCCee/flavour/B2Inv/apply_extra_cuts.py
@@ -15,9 +15,6 @@ def read_data(inpath, folder, cut, chunk_index=0):
     
     # read file and load RDataFrame or event data
     file = ROOT.TFile(file_path, 'READ') 
-    df = ROOT.RDataFrame("events", file) 
-    df = df.Filter(cut)
-    print(f"Number of entries after PV cut: {df.Count().GetValue()}")
 
     # Fetch the TParameters as integers
     tparams = {}
@@ -27,13 +24,21 @@ def read_data(inpath, folder, cut, chunk_index=0):
         param = file.Get(param_name)
         tparams[param_name] = param.GetVal()
 
+     # Check if the TTree named "events" exists
+    if not file.GetListOfKeys().Contains("events"):
+        print(f"No events in: {file.GetName()}")
+        return None, tparams, chunk_processed
     
+    df = ROOT.RDataFrame("events", file) 
+    df = df.Filter(cut)
+    print(f"Number of entries after PV cut: {df.Count().GetValue()}")
+
     return df, tparams,chunk_processed
 
 tupling_output_path = "/r02/lhcb/ejnw2/fcc_2025/FCCAnalyses/examples/FCCee/flavour/B2Inv/outputs/" 
 data_folder = "prelim_cuts_noPV_ntracks_cut" 
 input = os.path.join(os.path.abspath(tupling_output_path), data_folder)
-decay_list = cfg.samples
+decay_list =  ["p8_ee_Zmumu_ecm91","p8_ee_Zee_ecm91",] #cfg.samples
 cuts = "Rec_PV_ntracks>1"
 
 
@@ -45,65 +50,92 @@ for decay in decay_list:
         # Read data and TParameters (eventsProcessed, eventsSelected) for each decay sample
         df, tparams, chunk= read_data(inpath=input, folder=decay, cut=cuts, chunk_index=index)
        
+        if df:
 
-       # Define a new column that checks the hemisphere assignment between vtx_p and PV-SV vector matches
-        df = df.Define("matching_vtx_assignment", "Rec_vtx_in_hemisEmin==Rec_vtx_in_hemisEmin_d2PV")
-       
-        # Define a new column to check the condition [0, any number of 1s]
-        df = df.Define("HasFormZeroOnes", "matching_vtx_assignment.size() > 0 && matching_vtx_assignment.at(0) == 0 && Sum(matching_vtx_assignment) == matching_vtx_assignment.size() - 1")
-                
-        # Filter the DataFrame based on the matching condition
-        filtered_df = df.Filter("HasFormZeroOnes==1")
-
-
-        # Count the total events after the cuts
-        events_selected_after_both_cuts = filtered_df.Count().GetValue()
-
-        # Create TParameters for event counts
-        filtered_tparams = {
-            "eventsProcessed":  tparams["eventsProcessed"],
-            "eventsSelectedbyTupling": tparams["eventsSelected"],
-            "eventsSelected": events_selected_after_both_cuts,
-        }
+            # Define a new column that checks the hemisphere assignment between vtx_p and PV-SV vector matches
+            df = df.Define("matching_vtx_assignment", "Rec_vtx_in_hemisEmin==Rec_vtx_in_hemisEmin_d2PV")
         
-        # Print the results
-        print(f'Chunk: {chunk}')
-        print(f'Total events processed by tupling: {tparams["eventsProcessed"]}')
-        print(f'Total events selected by tupling: {tparams["eventsSelected"]}')
-        print(f'Events selected after two cuts: {events_selected_after_both_cuts}')
-        print(f'Fraction left after two cuts: {events_selected_after_both_cuts/tparams["eventsSelected"]}')
-    
+            # Define a new column to check the condition [0, any number of 1s]
+            df = df.Define("HasFormZeroOnes", "matching_vtx_assignment.size() > 0 && matching_vtx_assignment.at(0) == 0 && Sum(matching_vtx_assignment) == matching_vtx_assignment.size() - 1")
+                    
+            # Filter the DataFrame based on the matching condition
+            filtered_df = df.Filter("HasFormZeroOnes==1")
 
-        # Save the filtered data to a new ROOT file
-    
-        # Check if the folder exists
-        output_folder_path = os.path.join(os.path.abspath(tupling_output_path), "prelim_and_PVreco_vtxassignment_cuts")
-        
-        if not os.path.exists(output_folder_path):
-            # Create the folder if it doesn't exist
-            os.makedirs(output_folder_path)
 
-        if not os.path.exists(os.path.join(os.path.abspath(output_folder_path), decay)):
-            # Create the folder if it doesn't exist
-            os.makedirs(os.path.join(os.path.abspath(output_folder_path), decay))
+            # Count the total events after the cuts
+            events_selected_after_both_cuts = filtered_df.Count().GetValue()
+
+            # Create TParameters for event counts
+            filtered_tparams = {
+                "eventsProcessed":  tparams["eventsProcessed"],
+                "eventsSelectedbyTupling": tparams["eventsSelected"],
+                "eventsSelected": events_selected_after_both_cuts,
+            }
             
-        output_file_path = os.path.join(os.path.abspath(output_folder_path), decay, chunk +".root")
-
-        # Create a new ROOT file
-        output_file = ROOT.TFile(output_file_path, 'RECREATE')
-
-        # Save the RDataFrame as a TTree in the ROOT file
-        filtered_df.Snapshot("events", output_file_path)  
-
-        # Open the same ROOT file in UPDATE mode to write the TParameters
-        output_file = ROOT.TFile(output_file_path, 'UPDATE')
+            # Print the results
+            print(f'Chunk: {chunk}')
+            print(f'Total events processed by tupling: {tparams["eventsProcessed"]}')
+            print(f'Total events selected by tupling: {tparams["eventsSelected"]}')
+            print(f'Events selected after two cuts: {events_selected_after_both_cuts}')
+            print(f'Fraction left after two cuts: {events_selected_after_both_cuts/tparams["eventsSelected"]}')
         
-        # Write the TParameters as separate objects to the ROOT file
-        for param_name, param_values in filtered_tparams.items():
-            param = ROOT.TParameter('int')(param_name, param_values)
-            param.Write() 
 
-        output_file.Close()
+            # Save the filtered data to a new ROOT file
+            # Check if the folder exists
+            output_folder_path = os.path.join(os.path.abspath(tupling_output_path), "prelim_and_PVreco_vtxassignment_cuts")
+            
+            if not os.path.exists(output_folder_path):
+                # Create the folder if it doesn't exist
+                os.makedirs(output_folder_path)
+
+            if not os.path.exists(os.path.join(os.path.abspath(output_folder_path), decay)):
+                # Create the folder if it doesn't exist
+                os.makedirs(os.path.join(os.path.abspath(output_folder_path), decay))
+                
+            output_file_path = os.path.join(os.path.abspath(output_folder_path), decay, chunk +".root")
+
+            # Create a new ROOT file
+            output_file = ROOT.TFile(output_file_path, 'RECREATE')
+
+            # Save the RDataFrame as a TTree in the ROOT file
+            filtered_df.Snapshot("events", output_file_path)  
+
+            # Open the same ROOT file in UPDATE mode to write the TParameters
+            output_file = ROOT.TFile(output_file_path, 'UPDATE')
+            
+            # Write the TParameters as separate objects to the ROOT file
+            for param_name, param_values in filtered_tparams.items():
+                param = ROOT.TParameter('int')(param_name, param_values)
+                param.Write() 
+
+            output_file.Close()
+
+        else:
+            # Save the filtered data to a new ROOT file
+            # Check if the folder exists
+            output_folder_path = os.path.join(os.path.abspath(tupling_output_path), "prelim_and_PVreco_vtxassignment_cuts")
+            
+            if not os.path.exists(output_folder_path):
+                # Create the folder if it doesn't exist
+                os.makedirs(output_folder_path)
+
+            if not os.path.exists(os.path.join(os.path.abspath(output_folder_path), decay)):
+                # Create the folder if it doesn't exist
+                os.makedirs(os.path.join(os.path.abspath(output_folder_path), decay))
+                
+            output_file_path = os.path.join(os.path.abspath(output_folder_path), decay, chunk +".root")
+
+            # Create a new ROOT file
+            output_file = ROOT.TFile(output_file_path, 'RECREATE')
+            
+            # Write the TParameters as separate objects to the ROOT file
+            for param_name, param_values in tparams.items():
+                param = ROOT.TParameter('int')(param_name, param_values)
+                param.Write() 
+
+            output_file.Close()
+
+
 
 
 

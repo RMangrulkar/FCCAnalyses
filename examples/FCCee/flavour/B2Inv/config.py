2c5fc6265b0c32c15e09d3e4b8e6d7b0aa86cefb
Ella Wood <ella.wood@mac.com>
2025-02-17 23:49:52 +0000

    preparing config and B2Inv to run full data

--- examples/FCCee/flavour/B2Inv/config.py
+++ examples/FCCee/flavour/B2Inv/config.py
@@ -9,14 +9,14 @@ FCCAnalysesPath = "/r02/lhcb/ejnw2/fcc_2025/FCCAnalyses/examples/FCCee/flavour/B
 FCCAnalysesPath = os.path.abspath(FCCAnalysesPath)
 SavedOutputsPath = "/r02/lhcb/ejnw2/FCC_outputs_2024/outputs" #this is where old BDTs are saved
 # RUNNING MODE
-run_mode_choices = ['no_selection','prelim_cuts'] #when add run mode, now need to add to processList, fccana_opts
+run_mode_choices = ['no_selection','prelim_cuts','prelim_cuts_full'] #when add run mode, now need to add to processList, fccana_opts
 
 #BDTh - single hadronic BDT, used to separate signal from all hadronic bkgs in one go
 #BDTl - BDT to discriminate against light hadronic bkgs (u,d,s)
 #BDTmE - BDT to look for missing energy events in events that pass BDTl
 
 
-run_mode = 'prelim_cuts'
+run_mode = 'prelim_cuts_full'
 if run_mode not in run_mode_choices:
     raise RuntimeError(f'{run_mode} is not a valid run mode')
 
@@ -51,7 +51,7 @@ processList = {
     },
     
 
-    "prelim_cuts": {  # ~2G or ~500k events per sample #ALL NUMBERS TO UPDATE
+    "prelim_cuts": {  # ~2G or ~500k events per sample 
         "p8_ee_Zbb_ecm91_EvtGen_Bs2NuNu":{"fraction": 0.32, "chunks": 5},
         "p8_ee_Zbb_ecm91_EvtGen_Bd2NuNu": {"fraction": 0.30, "chunks": 5},
         "p8_ee_Zbb_ecm91": {"fraction": 0.028, "chunks": 16},
@@ -60,21 +60,20 @@ processList = {
         "p8_ee_Zud_ecm91": {"fraction": 0.048, "chunks": 32},
         #"p8_ee_Ztautau_ecm91": {"fraction": 1., "chunks": 40}, 
         #"p8_ee_Zmumu_ecm91": {"fraction": 1., "chunks": 100},
-        #"p8_ee_Zee_ecm91": {"fraction": 1., "chunks": 100},
-        
+        #"p8_ee_Zee_ecm91": {"fraction": 1., "chunks": 100},    
     },
-        #"prelim_cuts": {  # ~2G or ~500k events per sample #ALL NUMBERS TO UPDATE
-        #"p8_ee_Zbb_ecm91_EvtGen_Bs2NuNu":{"fraction": 1, "chunks": 20},
-        #"p8_ee_Zbb_ecm91_EvtGen_Bd2NuNu": {"fraction": 1, "chunks": 20},
-        #"p8_ee_Zbb_ecm91": {"fraction": 1, "chunks": 500},
-        #"p8_ee_Zcc_ecm91": {"fraction": 1, "chunks": 500},
-        #"p8_ee_Zss_ecm91": {"fraction": 1, "chunks": 500},
-        #"p8_ee_Zud_ecm91": {"fraction": 1, "chunks": 600},
-        #"p8_ee_Ztautau_ecm91": {"fraction": 1., "chunks": 40}, 
-        #"p8_ee_Zmumu_ecm91": {"fraction": 1., "chunks": 100},
-        #"p8_ee_Zee_ecm91": {"fraction": 1., "chunks": 100},
 
-    #},
+    "prelim_cuts_full": {  # processing all data
+        "p8_ee_Zbb_ecm91_EvtGen_Bs2NuNu":{"fraction": 1, "chunks": 20},
+        "p8_ee_Zbb_ecm91_EvtGen_Bd2NuNu": {"fraction": 1, "chunks": 20},
+        "p8_ee_Zbb_ecm91": {"fraction": 1, "chunks": 500},
+        "p8_ee_Zcc_ecm91": {"fraction": 1, "chunks": 500},
+        "p8_ee_Zss_ecm91": {"fraction": 1, "chunks": 500},
+        "p8_ee_Zud_ecm91": {"fraction": 1, "chunks": 600},
+        "p8_ee_Ztautau_ecm91": {"fraction": 1., "chunks": 50}, 
+        "p8_ee_Zmumu_ecm91": {"fraction": 1., "chunks": 100},
+        "p8_ee_Zee_ecm91": {"fraction": 1., "chunks": 100},
+    },
 
 
 }
@@ -84,7 +83,8 @@ fccana_opts = {
     "prodTag":   "FCCee/winter2023/IDEA",
     "outputDir": {
         "no_selection": os.path.join(FCCAnalysesPath, "outputs/no_selection/"),
-        "prelim_cuts": os.path.join(FCCAnalysesPath, "outputs/full_prelim_cuts/"),
+        "prelim_cuts": os.path.join(FCCAnalysesPath, "outputs/prelim_cuts/"),
+        "prelim_cuts_full": os.path.join(FCCAnalysesPath, "outputs/prelim_cuts_full_data/"),
         "stage1_training": os.path.join(FCCAnalysesPath, "outputs/stage1_training/"),
     },
     "testFile": {
@@ -108,7 +108,7 @@ fccana_opts = {
     "outputBranches": {
         "no_selection":"full-vars",
         "prelim_cuts": "full-vars",
-
+        "prelim_cuts_full": "full-vars",
     },
 }
 

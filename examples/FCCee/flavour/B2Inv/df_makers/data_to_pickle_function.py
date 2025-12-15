78c49d99d67d43a2fac607425f7e6f93f8709a64
Ella Wood <ella.wood@mac.com>
2025-12-15 22:34:37 +0000

    path changing for regoanisiation

--- examples/FCCee/flavour/B2Inv/df_makers/data_to_pickle_function.py
+++ examples/FCCee/flavour/B2Inv/df_makers/data_to_pickle_function.py
@@ -15,7 +15,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import config as cfg 
 from basic_functions import vars_fromyaml, check_inputpath, set_outputpath, chunk_list
 from efficiency_tools import efficiency_finder
-from apply_bdt_and_pickle_df import load_bdt_and_apply
+from df_makers.apply_bdt_and_pickle_df import load_bdt_and_apply
 
 #ROOT.EnableImplicitMT()
 ROOT.DisableImplicitMT()    # disable ROOT parallel threads
